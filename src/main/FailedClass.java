2faac315261bc8f40678f1a12f85f19cdc4ccc2e
p.lyahov <p.lyahov@a1qa.com>
2023-01-30 17:52:28 +0300

    add failed test

--- src/main/FailedClass.java
+++
@@ -1,5 +0,0 @@
-package com.dev3l.hello_world.test;
-
-public class FailedClass {
-    final static Str
-}
---
+++ src/test/java/com/dev3l/hello_world/test/FailedTest.java
@@ -0,0 +1,14 @@
+package com.dev3l.hello_world.test;
+
+import org.junit.Test;
+
+
+
+
+
+public class FailedTest {
+    @Test
+    public void doesntCompile() {
+        asdewcwc
+    }
+}
