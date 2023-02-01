3108343cca3b73dffe250d332a7ada4a68e0e32e
p.lyahov <p.lyahov@a1qa.com>
2023-02-02 02:59:39 +0300

    rename folder

--- src/test/java/com/dev3l/hello_world/test/FailedTest.java
+++ src/test/java/com/dev3l/hello_world/test/FailedTest.java
@@ -1,14 +1,24 @@
 package com.dev3l.hello_world.test;
 
 import org.junit.Test;
-
-
-
+import utils.Util;
 
 
 public class FailedTest {
     @Test
     public void doesntCompile() {
+        try {  Util.depricatedMethod();}
+        catch (Throwable t) {
+            System.out.println("Catch error");
+        }
+        myPow(1, 2);
+        Util.depricatedMethod();
+        Util.depricatedMethod();
+        Util.depricatedMethod();
+    }
 
+    public static int myPow(int num, int exponent) {
+        //num = num * myPow(num, exponent - 1); // Noncompliant
+        return 0;  // this is never reached
     }
 }
